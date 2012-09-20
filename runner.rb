028beda0a242a03dc4274ad79cf0b114e1186e6e
tc <tainacaetano@gmail.com>
2012-09-20 11:40:45 -0300

    takes the number of similar mixes from ARGV

--- runner.rb
+++ runner.rb
@@ -1,4 +1,13 @@
 require 'require_all'
 require_all 'lib'
 
-MixProvider.new(Client.new, ResponseParser.new).mix(ARGV[0]).save
+mix_provider = MixProvider.new(Client.new, ResponseParser.new)
+number_of_mixes_to_download = ARGV[1] || 0
+
+mix = mix_provider.mix(ARGV[0])
+mix.save
+
+number_of_mixes_to_download.to_i.times do 
+  mix = mix_provider.mix(mix.similar)
+  mix.save
+end
