feb60d265a821bce4ee784aa7cca857b3441e79e
tc <tainacaetano@gmail.com>
2012-09-21 18:36:10 -0300

    renanzinho

--- renan_solr.rb
+++ renan_solr.rb
@@ -5,8 +5,10 @@ client = Client.new
 parser = ResponseParser.new
 mix_provider = MixProvider.new(client, parser)
 
-number_of_mixes_to_download = ARGV[1] || 1
+number_of_mixes_to_download = ARGV[0] || 1
 
 mixes = parser.popular_playlists(client.popular_playlists number_of_mixes_to_download)
 
-mixes.each {|m| mix_provider.mix(m).to_json}
+file = File.open("renanzinho.txt", "w")
+mixes.each {|m| mix_provider.mix(m).songs.each{|s| file.puts(s.metadata.to_json)}; puts ""}
+file.close
